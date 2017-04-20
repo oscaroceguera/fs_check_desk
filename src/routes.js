87145c88dcd379e3f2472307af1081fd21f0f8cc
oscaroceguera <oscaroceguera@gmail.com>
2017-04-20 02:53:31 -0500

    [Routes] ::: Children routes for Singup & Login

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 
 import React from 'react'
 import { Router, Route, IndexRoute } from 'react-router'
-import App from './containers/App/App';
+import App from './components/App/App';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
 import HomeContainer from './containers/Home/HomeContainer'
 
 export default function getRoutes (history) {
@@ -9,7 +11,10 @@ export default function getRoutes (history) {
     <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={HomeContainer} />
-        <Route path="home" component={HomeContainer} />
+        <Route path="home" component={HomeContainer}>
+          <Route path="signup"  component={Signup} />
+          <Route path="login"  component={Login} />
+        </Route>
       </Route>
     </Router>
   )
