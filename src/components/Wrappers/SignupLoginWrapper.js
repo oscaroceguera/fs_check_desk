666b2c7d6525a61cd3474a214a1e0b33b4b0e790
oscaroceguera <oscaroceguera@gmail.com>
2017-04-22 22:37:53 -0500

    [Component] ::: Wrapper for singup/login form

---
+++ src/components/Wrappers/SignupLoginWrapper.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const PapeStyled = styled.div`
+  width: 300px;
+  margin: 1em auto;
+  padding: 1em;
+`
+
+const SignupLoginWrapper = ({children}) => (
+  <PapeStyled>
+    {children}
+  </PapeStyled>
+)
+
+SignupLoginWrapper.propTypes = {
+  children: React.PropTypes.array.isRequired
+}
+
+export default SignupLoginWrapper
