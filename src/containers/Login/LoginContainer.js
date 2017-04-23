7fc9b7e7b7f794f17d195f99ec7e681598fa7706
oscaroceguera <oscaroceguera@gmail.com>
2017-04-23 00:27:23 -0500

    [Containers] ::: Signup & Login ErrorText valdiator

--- src/containers/Login/LoginContainer.js
+++ src/containers/Login/LoginContainer.js
@@ -3,18 +3,14 @@ import aux from '../../helpers/AuxFunctions'
 import SignupLoginWrapper from '../../components/Wrappers/SignupLoginWrapper'
 import GenericTextField from '../../components/Forms/GenericTextField'
 
-// TODO: confirmar password
-class SignupContainer extends Component {
+class LoginContainer extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
       fields : {
-        firstName: '',
-        secondName: '',
         email: '',
-        password: '',
-        confPassword: ''
+        password: ''
       }
     }
   }
@@ -29,21 +25,8 @@ class SignupContainer extends Component {
   }
 
   render () {
-    console.log('STATE', this.state);
     return (
       <SignupLoginWrapper>
-        <GenericTextField
-          title={'Nombre'}
-          name={'firstName'}
-          errorText={this.handleErrorText('firstName', 'txt')}
-          onChange={this.onChangeInput}
-        />
-        <GenericTextField
-          title={'Apellido'}
-          name={'secondName'}
-          errorText={this.handleErrorText('secondName', 'txt')}
-          onChange={this.onChangeInput}
-        />
         <GenericTextField
           title={'Email'}
           name={'email'}
@@ -57,16 +40,9 @@ class SignupContainer extends Component {
           errorText={this.handleErrorText('password', 'password')}
           onChange={this.onChangeInput}
         />
-        <GenericTextField
-          title={'Confirmar Password'}
-          name={'confPassword'}
-          type={'password'}
-          errorText={this.handleErrorText('confPassword', 'password')}
-          onChange={this.onChangeInput}
-        />
       </SignupLoginWrapper>
     )
   }
 }
 
-export default SignupContainer
+export default LoginContainer
