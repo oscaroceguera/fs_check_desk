a7e3d703622cbed1d9f19a4a47a8db5bb2c27b48
oscaroceguera <oscaroceguera@gmail.com>
2017-04-23 00:26:51 -0500

    [Component] ::: Add type for GenericTextField

--- src/components/Forms/GenericTextField.js
+++ src/components/Forms/GenericTextField.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {TextField} from 'material-ui'
 
-const GenericTextField = ({title, name, errorText, onChange}) => (
+const GenericTextField = ({title, name, errorText, onChange, type = 'text'}) => (
   <TextField
     floatingLabelText={title}
     name={name}
     errorText={errorText}
     style={{ width: '100%' }}
     onChange={onChange}
+    type={type}
   />
 )
 
@@ -17,7 +18,8 @@ GenericTextField.propTypes = {
   title: string.isRequired,
   name: string.isRequired,
   onChange: func.isRequired,
-  errorText: string.isRequired
+  errorText: string.isRequired,
+  type: string
 }
 
 export default GenericTextField
