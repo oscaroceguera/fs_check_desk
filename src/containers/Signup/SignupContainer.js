a9a58e23f25706eb86a743a7ef3d78198940eeb9
oscaroceguera <oscaroceguera@gmail.com>
2017-04-23 19:53:06 -0500

    [Container] ::: Compare loading is true

--- src/containers/Signup/SignupContainer.js
+++ src/containers/Signup/SignupContainer.js
@@ -2,66 +2,38 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as singupActions from '../../reducers/signupReducer'
-import SignupLoginWrapper from '../../components/Wrappers/SignupLoginWrapper'
-import GenericTextField from '../../components/Forms/GenericTextField'
-import GenericSubmit from '../../components/Forms/GenericSubmit'
 import aux from '../../helpers/AuxFunctions'
 import { passEqualsTxtMsg, showsubmit } from '../../selectors/signupSelector'
 
+import Signup from '../../components/Signup/Signup'
+import CircularLoading from '../../components/Progress/CircularLoading'
 
-// TODO: showsubmit
+// TODO: ADMISNTRAR LOADIN
+// TODO: MENSAJE DE SIGNUP_SUCCESS
+// TODO: MENSAJE DE SIGNUP_FAIL
 class SignupContainer extends Component {
-
   onChangeInput = (e) => {
     this.props.setFields('fields', e.target.name, e.target.value)
   }
-
-  handleErrorText(field, type) {
+  handleErrorText = (field, type) => {
     const { fields } = this.props
     return aux.errorTextMessage(fields[field], type)
   }
-
+  handleSignup = (e) => {
+    this.props.signupFanout()
+  }
   render () {
-    const { isPasswordEqual, disabled } = this.props
+    const { isPasswordEqual, disabled, savedLoading } = this.props
     return (
-      <SignupLoginWrapper>
-        <GenericTextField
-          title={'Nombre'}
-          name={'firstName'}
-          errorText={this.handleErrorText('firstName', 'txt')}
-          onChange={this.onChangeInput}
-        />
-        <GenericTextField
-          title={'Apellido'}
-          name={'secondName'}
-          errorText={this.handleErrorText('secondName', 'txt')}
-          onChange={this.onChangeInput}
-        />
-        <GenericTextField
-          title={'Email'}
-          name={'email'}
-          errorText={this.handleErrorText('email', 'email')}
-          onChange={this.onChangeInput}
-        />
-        <GenericTextField
-          title={'Password'}
-          name={'password'}
-          type={'password'}
-          errorText={this.handleErrorText('password', 'password')}
-          onChange={this.onChangeInput}
-        />
-        <GenericTextField
-          title={'Confirmar Password'}
-          name={'confPassword'}
-          type={'password'}
-          errorText={isPasswordEqual}
-          onChange={this.onChangeInput}
-        />
-      <GenericSubmit
-        disabled={disabled}
-        label={'Signup'}
-      />
-      </SignupLoginWrapper>
+      savedLoading
+        ? <CircularLoading />
+        : <Signup
+            errorTxtContainer={this.handleErrorText}
+            onChangeInput={this.onChangeInput}
+            handleSignup={this.handleSignup}
+            disabled={disabled}
+            isPasswordEqual={isPasswordEqual}
+          />
     )
   }
 }
@@ -71,7 +43,10 @@ const mapStateToProps = (state) => {
   return {
     fields: stateJS.fields,
     isPasswordEqual: passEqualsTxtMsg(state),
-    disabled: !showsubmit(state)
+    disabled: !showsubmit(state),
+    savedLoading: stateJS.savedLoading,
+    savedFail: stateJS.savedFail,
+    savedSucces: stateJS.savedSucces,
   }
 }
 
@@ -79,6 +54,15 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
     ...singupActions
   }, dispatch)
+}
+
+SignupContainer.propTypes = {
+  fields: React.PropTypes.object.isRequired,
+  isPasswordEqual: React.PropTypes.string.isRequired,
+  disabled: React.PropTypes.bool.isRequired,
+  savedLoading: React.PropTypes.bool.isRequired,
+  savedFail: React.PropTypes.any,
+  savedSucces: React.PropTypes.bool.isRequired,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupContainer)
