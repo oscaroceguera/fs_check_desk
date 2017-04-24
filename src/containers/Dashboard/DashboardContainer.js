7a5299699400969e708b1452ce70690466a224da
oscaroceguera <oscaroceguera@gmail.com>
2017-04-24 01:52:58 -0500

    [Container]  ::: Dashboard simple container was created

---
+++ src/containers/Dashboard/DashboardContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class DashboardContainer extends Component {
+  render () {
+    return <h1>EL SADHBOAR MI CAMARAS</h1>
+  }
+}
+
+export default DashboardContainer
