b8c942e45334436c17dbb81314a01d642f63742f
oscaroceguera <oscaroceguera@gmail.com>
2017-04-23 19:50:53 -0500

    [Component] ::: Signup component was created

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import SignupLoginWrapper from '../Wrappers/SignupLoginWrapper'
+import GenericTextField from '../Forms/GenericTextField'
+import GenericSubmit from '../Forms/GenericSubmit'
+
+const Signup = ({errorTxtContainer, onChangeInput, disabled, isPasswordEqual, handleSignup}) => {
+  return (
+    <SignupLoginWrapper>
+      <GenericTextField
+        title={'Nombre'}
+        name={'firstName'}
+        errorText={errorTxtContainer('firstName', 'txt')}
+        onChange={onChangeInput}
+      />
+      <GenericTextField
+        title={'Apellido'}
+        name={'secondName'}
+        errorText={errorTxtContainer('secondName', 'txt')}
+        onChange={onChangeInput}
+      />
+      <GenericTextField
+        title={'Email'}
+        name={'email'}
+        errorText={errorTxtContainer('email', 'email')}
+        onChange={onChangeInput}
+      />
+      <GenericTextField
+        title={'Password'}
+        name={'password'}
+        type={'password'}
+        errorText={errorTxtContainer('password', 'password')}
+        onChange={onChangeInput}
+      />
+      <GenericTextField
+        title={'Confirmar Password'}
+        name={'confPassword'}
+        type={'password'}
+        errorText={isPasswordEqual}
+        onChange={onChangeInput}
+      />
+    <GenericSubmit
+      disabled={disabled}
+      label={'Signup'}
+      onClick={handleSignup}
+    />
+    </SignupLoginWrapper>
+  )
+}
+
+// TODO: proptypes
+
+export default Signup
