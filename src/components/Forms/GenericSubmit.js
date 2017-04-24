c4c8f7ec28a2c886868910e2b64def75d544b82a
oscaroceguera <oscaroceguera@gmail.com>
2017-04-23 19:49:44 -0500

    [Component] ::: onclick event was added in Generic Submit

--- src/components/Forms/GenericSubmit.js
+++ src/components/Forms/GenericSubmit.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import RaisedButton from 'material-ui/RaisedButton';
 
-const GenericSubmit = ({disabled, label}) => (
+const GenericSubmit = ({disabled, label, onClick}) => (
   <div style={{ textAlign: 'center' }}>
-    <RaisedButton disabled={disabled} label={label}  primary/>
+    <RaisedButton
+      disabled={disabled}
+      label={label}
+      onClick={onClick}
+      primary/>
   </div>
 )
 
-const {bool, string} = React.PropTypes
+const { bool, string, func } = React.PropTypes
 
+// TODO: onClick is required
 GenericSubmit.propTypes = {
   disabled: bool.isRequired,
-  label: string.isRequired
+  label: string.isRequired,
+  onClick: func
 }
 
 export default GenericSubmit
