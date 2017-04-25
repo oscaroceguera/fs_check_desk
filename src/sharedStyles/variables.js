285048d381f04f8a0dbf3cb2bd9eeef2aee590e8
oscaroceguera <oscaroceguera@gmail.com>
2017-04-25 02:42:12 -0500

    [Styles] ::: Variables for inline styles

---
+++ src/sharedStyles/variables.js
@@ -0,0 +1,7 @@
+import * as Colors from 'material-ui/styles/colors'
+
+export const pink500 = Colors.pink500
+export const red500 = Colors.red500
+export const lightBlue900 = Colors.lightBlue900
+export const greenA700 = Colors.greenA700
+export const BX_SHADOW = '0px 0px 5px grey'
