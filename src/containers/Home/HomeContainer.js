e6d45d02e649a485e2105d5c9997df42e194fcff
oscaroceguera <oscaroceguera@gmail.com>
2017-04-26 23:02:09 -0500

    [Container] ::: When user is authenticated push to dashboard

--- src/containers/Home/HomeContainer.js
+++ src/containers/Home/HomeContainer.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Logo from '../../components/Logo/Logo'
 import LinkTitle from '../../components/Links/LinkTitle'
 import './styles.css'
 
+// TODO: Cuando este en el home y si ya esta logueado mandar a dasboard
+
 class HomeContainer extends Component {
+  static contextTypes = {
+    router: React.PropTypes.object
+  }
+
   static propTypes = {
     location: React.PropTypes.object
   }
+
+  componentWillMount () {
+    if (this.props.authenticated) {
+      this.context.router.push('/dashboard')
+    }
+  }
+
+  componentWillUpdate (nextProps) {
+    if (nextProps.authenticated) {
+      this.context.router.push('/dashboard')
+    }
+  }
+
   render () {
     const {location} = this.props
     return (
@@ -30,5 +50,11 @@ class HomeContainer extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  const stateJS = state.authReducer.toJS()
+  return {
+    authenticated: stateJS.authenticated
+  }
+}
 
-export default HomeContainer
+export default connect(mapStateToProps)(HomeContainer)
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -66,7 +66,7 @@ export function logoutUser () {
     dispatch(unAuthUser())
     localStorage.removeItem('token')
     localStorage.removeItem('user')
-    browserHistory.push('/home')
+    browserHistory.push('/')
   }
 }
 
