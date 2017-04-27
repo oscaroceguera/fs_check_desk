c1d069f170f4dcdde6ac7af46cd1799f5b20b3e8
oscaroceguera <oscaroceguera@gmail.com>
2017-04-27 01:36:52 -0500

    [Container] ::: circular loading & snackbar error

--- src/containers/Login/LoginContainer.js
+++ src/containers/Login/LoginContainer.js
@@ -4,6 +4,8 @@ import { bindActionCreators } from 'redux'
 import * as loginActions from '../../reducers/authReducer'
 import aux from '../../helpers/AuxFunctions'
 
+import SnackbarContainer from '../Snackbar/SnackbarContainer'
+import CircularLoading from '../../components/Progress/CircularLoading'
 import Login from '../../components/Login/Login'
 import { fieldsNotNull } from '../../selectors/loginSelector'
 
@@ -19,14 +21,21 @@ class LoginContainer extends Component {
     this.props.loginUser()
   }
   render () {
-    const { disabled } = this.props
+    const { disabled, loading, error } = this.props
     return (
-      <Login
-        disabled={disabled}
-        handleErrorText={this.handleErrorText}
-        onChangeInput={this.onChangeInput}
-        handleLogin={this.handleLogin}
-      />
+      <div>
+        {
+          loading
+            ? <CircularLoading />
+            : <Login
+                disabled={disabled}
+                handleErrorText={this.handleErrorText}
+                onChangeInput={this.onChangeInput}
+                handleLogin={this.handleLogin}
+              />
+        }
+        { error && <SnackbarContainer msg={error} /> }
+      </div>
     )
   }
 }
@@ -35,6 +44,8 @@ const mapStateToProps = (state) => {
   const stateJS = state.authReducer.toJS()
   return {
     fields: stateJS.fields,
+    loading: stateJS.loading,
+    error: stateJS.error,
     disabled: !fieldsNotNull(state)
   }
 }
@@ -45,9 +56,15 @@ const mapDispatchToProps = (dispatch) => {
   }, dispatch)
 }
 
+const { object, bool, string, func } = React.PropTypes
+
 LoginContainer.propTypes = {
-  fields: React.PropTypes.object.isRequired,
-  disabled: React.PropTypes.bool.isRequired
+  fields: object.isRequired,
+  disabled: bool.isRequired,
+  error: string,
+  loading: bool.isRequired,
+  setFields: func.isRequired,
+  loginUser: func.isRequired
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer)
