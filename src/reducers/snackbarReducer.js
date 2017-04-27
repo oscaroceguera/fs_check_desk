b63f5c11434c52d1ddb10c4fa784e07086d63798
oscaroceguera <oscaroceguera@gmail.com>
2017-04-27 01:32:26 -0500

    [Reducer] ::: Action "Open Snackbar" was created

--- src/reducers/snackbarReducer.js
+++ src/reducers/snackbarReducer.js
@@ -1,12 +1,10 @@
 import { fromJS } from 'immutable'
 
 const CLOSE_SNACKBAR = 'src/snackbar/CLOSE_SNACKBAR'
+const OPEN_SNACKBAR = 'src/snackbar/OPEN_SNACKBAR'
 
-export function closeSnackbar () {
-  return {
-    type: CLOSE_SNACKBAR
-  }
-}
+export const closeSnackbar = () => ({ type: CLOSE_SNACKBAR })
+export const openSnackbar = () => ({ type: OPEN_SNACKBAR })
 
 const initialState = fromJS({
   open: true
@@ -16,6 +14,8 @@ function snackbarReducer (state = initialState, action) {
   switch (action.type) {
     case CLOSE_SNACKBAR:
       return state.set('open', false)
+    case OPEN_SNACKBAR:
+      return state.set('open', true)
     default:
       return state
   }
