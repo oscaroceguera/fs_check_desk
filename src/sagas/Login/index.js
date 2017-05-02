193f83c146b9833e0c3de62ee9e3491ad006cdb3
oscaroceguera <oscaroceguera@gmail.com>
2017-05-02 01:30:34 -0500

    [Sagas] ::: Login and Logout sagas

---
+++ src/sagas/Login/index.js
@@ -0,0 +1,54 @@
+import { delay } from 'redux-saga'
+import { call, put, select, takeLatest } from 'redux-saga/effects'
+import { browserHistory } from 'react-router'
+import { login } from '../../helpers/api'
+import { LOGIN_USER, LOGOUT_USER, loginLoading, resetError, authUser, authUserFail, unAuthUser } from '../../reducers/authReducer'
+
+const setTokenLS = (token) => {
+  localStorage.setItem('token', token)
+}
+
+const setUserLS = (user) => {
+  localStorage.setItem('user', JSON.stringify(user))
+}
+
+const removeTokenLS = () => {
+  localStorage.removeItem('token')
+}
+
+const removeUserLS = () => {
+  localStorage.removeItem('user')
+}
+
+function* loginFanout () {
+  yield [put(resetError()), put(loginLoading())]
+  yield delay(1000)
+  const data = yield select((state) => state.authReducer.toJS().fields)
+  try {
+    const response = yield call(login, data)
+    yield [call(setTokenLS, response.data.token), call(setUserLS, response.data.user)]
+    yield put(authUser())
+    browserHistory.push('/dashboard')
+  } catch (err) {
+    yield put(authUserFail(err))
+  }
+
+}
+
+function* logoutFanout () {
+  yield delay(300)
+  yield put(unAuthUser())
+  yield [call(removeTokenLS), call(removeUserLS)]
+  browserHistory.push('/')
+}
+
+function* defaultSaga () {
+  yield [
+    takeLatest(LOGIN_USER, loginFanout),
+    takeLatest(LOGOUT_USER, logoutFanout)
+  ]
+}
+
+export const sagas = [
+  defaultSaga
+]
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,7 @@
 import { sagas as SignupSagas } from './Signup'
+import { sagas as LoginSagas } from './Login'
 
 export default [
-  ...SignupSagas
+  ...SignupSagas,
+  ...LoginSagas
 ]
