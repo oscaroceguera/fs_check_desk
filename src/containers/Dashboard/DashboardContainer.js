445f351d63540a7b9957d6f00321f0caaad10dd9
oscaroceguera <oscaroceguera@gmail.com>
2017-05-04 02:05:09 -0500

    [Container] ::: Drawer menu & Menu was added

--- src/containers/Dashboard/DashboardContainer.js
+++ src/containers/Dashboard/DashboardContainer.js
@@ -2,37 +2,71 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as loginActions from '../../reducers/authReducer'
+import * as dashboarActions from '../../reducers/dashboardReducer'
 
 import Appbar from '../../components/Appbar/Appbar'
+import DrawerMenu from '../../components/DrawerMenu/DrawerMenu'
+import MenuDesk from '../../components/MenuDesk/MenuDesk'
+
 
 class DashboardContainer extends Component {
+  // Handle logout
+  onLogout = () => this.props.logoutUser()
+
+  // Handle Drawer menu
+  onMenu = () => this.props.drawerMenu()
+
+  // Handle Drawer menu close
+  onMenuClose = () => this.props.drawerMenuClose()
+
   render () {
+    const { location } = this.props
     return (
       <div>
-        <Appbar logout={() => this.props.logoutUser()} />
+        <Appbar
+          logout={this.onLogout}
+          handleToggle={this.onMenu}
+        />
+        <div style={{ display: 'flex' }}>
+          <MenuDesk location={location.pathname}/>
+          <DrawerMenu
+            open={this.props.drawerOpen}
+            handleClose={this.onMenuClose}
+          />
+          <div style={{ flexGrow: 1, padding: '1.5em 3em' }}>
+            {this.props.children}
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
-  const stateJS = state.authReducer.toJS()
+  const auth = state.authReducer.toJS()
+  const dashboard = state.dashboardReducer.toJS()
   return {
-    profile: stateJS.profile
+    profile: auth.profile,
+    drawerOpen: dashboard.open
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
-    ...loginActions
+    ...loginActions,
+    ...dashboarActions
   }, dispatch)
 }
 
-const { func, object } = React.PropTypes
+const { func, object, bool } = React.PropTypes
 
 DashboardContainer.propTypes = {
   profile: object.isRequired,
-  logoutUser: func.isRequired
+  logoutUser: func.isRequired,
+  drawerOpen: bool.isRequired,
+  drawerMenu: func.isRequired,
+  drawerMenuClose: func.isRequired,
+  children: object.isRequired
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer)
