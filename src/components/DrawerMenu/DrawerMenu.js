6373b74fd05394126b3de39008fb3817dd234c6a
oscaroceguera <oscaroceguera@gmail.com>
2017-05-04 02:56:50 -0500

    [Component] ::: Links functionality were added in menu

--- src/components/DrawerMenu/DrawerMenu.js
+++ src/components/DrawerMenu/DrawerMenu.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Drawer, MenuItem } from 'material-ui'
+import LinkMenuDesk from '../Links/LinkMenuDesk'
 
-const DrawerMenu = ({ open, handleClose }) => (
-  <Drawer open={open}>
-    <MenuItem onTouchTap={handleClose}>Menu Item</MenuItem>
-    <MenuItem onTouchTap={handleClose}>Menu Item 2</MenuItem>
+const DrawerMenu = ({ open, handleClose, links, location }) => (
+  <Drawer open={open} width={150}>
+    {
+      links.map(({path, label}, key) =>  (
+        <MenuItem key={key} onTouchTap={handleClose}>
+          <LinkMenuDesk key={key} location={location} path={path} label={label}/>
+        </MenuItem>
+      ))
+    }
   </Drawer>
 )
 
 DrawerMenu.propTypes = {
   open: React.PropTypes.bool.isRequired,
-  handleClose: React.PropTypes.func.isRequired
+  handleClose: React.PropTypes.func.isRequired,
+  links: React.PropTypes.array.isRequired,
+  location: React.PropTypes.string.isRequired
 }
 
 export default DrawerMenu
