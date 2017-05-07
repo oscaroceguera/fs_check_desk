b61b974274c89509fcbf394e77f1dc058aa78b6d
oscaroceguera <oscaroceguera@gmail.com>
2017-05-06 23:03:10 -0500

    [Component] ::: Dashboard component was created

---
+++ src/components/Wrappers/DashboardWrapper.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+import breakpoint from 'styled-components-breakpoint'
+
+const Wrapper = styled.div`
+  margin: auto;
+  max-width: 530px;
+  margin: auto;
+  ${breakpoint('desktop')`
+    max-width: 800px;
+  `}
+`
+const WrapperTitle = styled.h1`
+  text-align: center;
+`
+const WrapperTitleDesc = styled.span`
+  font-size: 12px;
+  display: block;
+  color: gray;
+  font-weight: normal;
+`
+
+const DashboardWrapper = ({title, desc, children}) => (
+  <Wrapper>
+    <WrapperTitle>{title}
+      <WrapperTitleDesc>{desc}</WrapperTitleDesc>
+    </WrapperTitle>
+    {children}
+  </Wrapper>
+)
+
+DashboardWrapper.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  desc: React.PropTypes.string.isRequired,
+  children: React.PropTypes.object.isRequired,
+}
+
+export default DashboardWrapper
