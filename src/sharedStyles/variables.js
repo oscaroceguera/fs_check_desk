caefa0826da877eea043078c4414db3fbbfa49b5
oscaroceguera <oscaroceguera@gmail.com>
2017-05-06 22:56:32 -0500

    [Styles] ::: Asign grey and teal color to variables

--- src/sharedStyles/variables.js
+++ src/sharedStyles/variables.js
@@ -4,4 +4,7 @@ export const pink500 = Colors.pink500
 export const red500 = Colors.red500
 export const lightBlue900 = Colors.lightBlue900
 export const greenA700 = Colors.greenA700
+export const tealA200 = Colors.tealA200
+export const grey800 = Colors.grey800
+export const grey700 = Colors.grey700
 export const BX_SHADOW = '0px 0px 5px grey'
