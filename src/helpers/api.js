10256192b14f1a8dd62cf3a467ca1e318d4846a1
oscaroceguera <oscaroceguera@gmail.com>
2017-05-10 00:26:17 -0500

    [Helpers] ::: PostSchema and config headers

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 const api = 'http://localhost:8000/api'
 
+const headers = {
+  headers: { 'Authorization': localStorage.getItem('token') }
+}
+
 export const signupUser = (data) =>
   axios
     .post(`${api}/auth/register`, data)
@@ -12,7 +16,6 @@ export const login = (data) =>
     .then((response) => response)
 
 export const getSchemas = () => [
-  { id: '9708fa76-001c-4fe2-8a1f-b1b5099f5199', name: 'SENASICA BUMA GLOBAL GAGLOBALB GLOBAL GLOBAL', version: '1.2', description: 'Design Patterns is a library with its code with a lightweight 3D library for transferring data fetching. VueJS is a high-level.  VueJS is a.' },
   { id: '6a1129cc-5cc1-4c1d-9efe-1cb6dbed3199', name: 'SENASICA', version: '1.2', description: 'Design Patterns is a library with its code with a lightweight 3D library for transferring data fetching. VueJS is a high-level.  VueJS is a.' },
   { id: 'a413210f-74a4-454e-9ca7-cbe315b178ca', name: 'Primus gfs', version: '1.2', description: 'Design Patterns is a library with its code with a lightweight 3D library for transferring data fetching. VueJS is a high-level.  VueJS is a.' },
   { id: '4e61907d-adc4-420e-b597-332c6031c22c', name: 'SENASICA BUMA GLOBAL GAB SENASICA BUMA GLOBAL GAB SENASICA BUMA GLOBAL GAB', version: '1.2', description: 'Design Patterns is a library with its code with a lightweight 3D library for transferring data fetching. VueJS is a high-level.  VueJS is a.' },
@@ -21,3 +24,8 @@ export const getSchemas = () => [
   { id: '32df06bc-8194-4232-be57-822dabb06e6f', name: 'SENASICA BUMA GLOBAL GAB', version: '1.2', description: 'Design Patterns is a library with its code with a lightweight 3D library for transferring data fetching. VueJS is a high-level.  VueJS is a.' },
   { id: '888a967b-ba43-4e7b-b6ab-32262825a314', name: 'SENASICA BUMA GLOBAL GAB SENASICA BUMA GLOBAL GAB SENASICA BUMA GLOBAL GAB', version: '1.2', description: 'Design Patterns is a library with its code with a lightweight 3D library for transferring data fetching. VueJS is a high-level.  VueJS is a.' }
 ]
+
+export const postSchema = (data) =>
+  axios
+    .post(`${api}/auth/schema`, data, headers)
+    .then((response) => response.data)
