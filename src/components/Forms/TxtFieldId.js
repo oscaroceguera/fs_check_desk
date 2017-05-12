bab5e23247a9e6cf18f5e6b643685a095ad2a78e
oscaroceguera <oscaroceguera@gmail.com>
2017-05-12 00:27:01 -0500

    [Components] ::: Text field id was created

---
+++ src/components/Forms/TxtFieldId.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {TextField} from 'material-ui'
+
+const TxtFieldId = ({ item }) => (
+  <TextField
+    style={{ display: item.id ? 'block' : 'none ', width: '310px' }}
+    floatingLabelText={'ID'}
+    value={item.id ? item.id : ''}
+    disabled={true}
+  />
+)
+
+TxtFieldId.propTypes = {
+  item: React.PropTypes.object.isRequired
+}
+
+export default TxtFieldId
