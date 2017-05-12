e358591fb5143119782b5a547b5aace2a542a496
oscaroceguera <oscaroceguera@gmail.com>
2017-05-12 01:05:12 -0500

    [Sagas] ::: import saved success and fail

--- src/sagas/Schema/index.js
+++ src/sagas/Schema/index.js
@@ -1,6 +1,10 @@
 import { delay } from 'redux-saga'
 import { call, put, select, takeLatest } from 'redux-saga/effects'
-import { FETCH_SCHEMAS, SET_SAVED_SCHEMA, fetchSchemasLoading, fetchSchemaSuccess, fetchSchemasFail } from '../../reducers/schemasReducer'
+import {
+  FETCH_SCHEMAS, SET_SAVED_SCHEMA,
+  fetchSchemasLoading, fetchSchemaSuccess, fetchSchemasFail,
+  setSavedSchemaLoading, setSavedSchemaSuccess, setSavedSchemaFail
+} from '../../reducers/schemasReducer'
 import { getSchemas, postSchema } from '../../helpers/api'
 
 function* fetchSchemas() {
@@ -15,12 +19,21 @@ function* fetchSchemas() {
 }
 
 function* savedSchema() {
+  yield put(setSavedSchemaLoading())
   const data = yield select((state) => state.schemasReducer.toJS().schema)
+  yield delay(1000)
   try {
     const schema = yield call(postSchema, data)
     console.log('Schema', schema);
+    const _schema = {
+      id: schema._id,
+      name: schema.name,
+      version: schema.version,
+      description: schema.description
+    }
+    yield put(setSavedSchemaSuccess(_schema))
   } catch (err) {
-    console.log('error', err);
+    yield put(setSavedSchemaFail(err))
   }
 }
 
