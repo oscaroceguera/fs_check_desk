d26bfd1f3717529763e116aa909cbd3a163fcf62
oscaroceguera <oscaroceguera@gmail.com>
2017-05-13 02:10:38 -0500

    [Reducer/Saga] ::: Removed GetSchemas

--- src/reducers/schemasReducer.js
+++ src/reducers/schemasReducer.js
@@ -1,10 +1,7 @@
-import { fromJS, List } from 'immutable'
+import { fromJS } from 'immutable'
 
 const SET_FIELDS = 'src/schemas/SET_FIELDS'
-export const FETCH_SCHEMAS = 'src/schemas/FETCH_SCHEMAS'
-export const FETCH_SCHEMAS_LOADING = 'src/schemas/FETCH_SCHEMAS_LOADING'
-export const FETCH_SCHEMAS_SUCCESS = 'src/schemas/FETCH_SCHEMAS_SUCCESS'
-export const FETCH_SCHEMAS_FAIL = 'src/schemas/FETCH_SCHEMAS_FAIL'
+const RESET_FIELDS = 'src/schemas/RESET_FIELDS'
 
 export const SET_SAVED_SCHEMA = 'src/schemas/SET_SAVED_SCHEMA'
 export const SET_SAVED_SCHEMA_LOADING = 'src/schemas/SET_SAVED_SCHEMA_LOADING'
@@ -12,48 +9,26 @@ export const SET_SAVED_SCHEMA_SUCCESS = 'src/schemas/SET_SAVED_SCHEMA_SUCCESS'
 export const SET_SAVED_SCHEMA_FAIL = 'src/schemas/SET_SAVED_SCHEMA_FAIL'
 
 export const setFields = (section, item, value) => ({ type: SET_FIELDS, section, item, value })
-export const fetchSchemas = () => ({ type: FETCH_SCHEMAS })
-export const fetchSchemasLoading = () => ({ type: FETCH_SCHEMAS_LOADING })
-export const fetchSchemaSuccess = (schemas) => ({ type: FETCH_SCHEMAS_SUCCESS, schemas })
-export const fetchSchemasFail = (err) => ({ type: FETCH_SCHEMAS_FAIL, err })
-
+export const resetFields = () => ({ type: RESET_FIELDS })
 export const setSavedSchema = () => ({ type: SET_SAVED_SCHEMA })
 export const setSavedSchemaLoading = () => ({ type: SET_SAVED_SCHEMA_LOADING })
 export const setSavedSchemaSuccess = (schema) => ({ type: SET_SAVED_SCHEMA_SUCCESS, schema })
 export const setSavedSchemaFail = (err) => ({ type: SET_SAVED_SCHEMA_FAIL, err })
 
 const initialState = fromJS({
-  schemas: [],
-  schemasLoading: false,
-  schemasFail: null,
   schema: {
     name: '',
     version: '',
     description: ''
-  },
-  savedSchemasLoading: false,
-  savedSchemaFail: null
+  }
 })
 
 function schemasReducer (state = initialState, action) {
   switch (action.type) {
     case SET_FIELDS:
       return state.setIn([action.section, action.item], action.value)
-    case FETCH_SCHEMAS_LOADING:
-      return state.merge({
-        schemasLoading: true,
-        schemasFail: null
-      })
-    case FETCH_SCHEMAS_SUCCESS:
-      return state.merge({
-        schemasLoading: false,
-        schemas: List.of(...action.schemas)
-      })
-    case FETCH_SCHEMAS_FAIL:
-      return state.merge({
-        schemasLoading: false,
-        schemasFail: action.err
-      })
+    case RESET_FIELDS:
+      return state.merge(initialState)
     case SET_SAVED_SCHEMA_LOADING:
       return state.merge({
         savedSchemasLoading: true,
--- src/sagas/Schema/index.js
+++ src/sagas/Schema/index.js
@@ -1,30 +1,20 @@
 import { delay } from 'redux-saga'
 import { call, put, select, takeLatest } from 'redux-saga/effects'
 import {
-  FETCH_SCHEMAS, SET_SAVED_SCHEMA,
-  fetchSchemasLoading, fetchSchemaSuccess, fetchSchemasFail,
-  setSavedSchemaLoading, setSavedSchemaSuccess, setSavedSchemaFail
+  SET_SAVED_SCHEMA,
+  setSavedSchemaLoading,
+  setSavedSchemaSuccess,
+  setSavedSchemaFail
 } from '../../reducers/schemasReducer'
-import { getSchemas, postSchema } from '../../helpers/api'
+import { postSchema } from '../../helpers/api'
 
-function* fetchSchemas() {
-  yield put(fetchSchemasLoading())
-  yield delay(1000)
-  try {
-    const schemas = yield call(getSchemas)
-    yield put(fetchSchemaSuccess(schemas))
-  } catch (err) {
-    yield put(fetchSchemasFail(err))
-  }
-}
 
 function* savedSchema() {
   yield put(setSavedSchemaLoading())
   const data = yield select((state) => state.schemasReducer.toJS().schema)
   yield delay(1000)
   try {
-    const schema = yield call(postSchema, data)
-    console.log('Schema', schema);
+    const schema = yield call(postSchema, data, localStorage.getItem('token'))
     const _schema = {
       id: schema._id,
       name: schema.name,
@@ -39,7 +29,6 @@ function* savedSchema() {
 
 function* defaultSaga () {
   yield [
-    takeLatest(FETCH_SCHEMAS, fetchSchemas),
     takeLatest(SET_SAVED_SCHEMA, savedSchema)
   ]
 }
