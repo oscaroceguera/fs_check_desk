6e3dee5c962d6b5e9544f62912f5d1be6edd98fc
oscaroceguera <oscaroceguera@gmail.com>
2017-05-13 02:06:34 -0500

    [Components] ::: There arent Schemas & there are schemas components

--- src/components/Cards/SchemaCard.js
+++ src/components/Cards/SchemaCard.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import {CardContainer, Card, CardHeader, CardHeaderTitle, CardHeaderVersion, CardDescription} from './styles'
+import { grey400 } from '../../sharedStyles/variables'
 
-const SchemaCard = ({schemas}) => (
+const H2_STYLE = {
+  margin: 'auto',
+  color: grey400
+}
+
+const ThereArentSchemas = () => <h2 style={H2_STYLE}>{'¡No existen esquemas registrados aún!'}</h2>
+
+const ThereAreSchemas = ({ item }) => (
+  <Card>
+    <CardHeader>
+      <CardHeaderTitle>{item.name}</CardHeaderTitle>
+      <CardHeaderVersion>{`Version: ${item.version}`}</CardHeaderVersion>
+    </CardHeader>
+    <div>
+      <CardDescription>{item.description}</CardDescription>
+    </div>
+  </Card>
+)
+
+ThereAreSchemas.propTypes = { item: React.PropTypes.object.isRequired }
+
+const SchemaCard = ({ schemas }) => (
   <CardContainer>
-    {
-      schemas.map((item, key) => (
-        <Card key={key}>
-          <CardHeader>
-            <CardHeaderTitle>{item.name}</CardHeaderTitle>
-            <CardHeaderVersion>{`Version: ${item.version}`}</CardHeaderVersion>
-          </CardHeader>
-          <div>
-            <CardDescription>{item.description}</CardDescription>
-          </div>
-        </Card>
-      ))
-    }
+    { schemas.length > 0 ? schemas.map((item, key) => <ThereAreSchemas key={key} item={item} />) : <ThereArentSchemas /> }
   </CardContainer>
 )
 
-// TODO: Proptypes
+SchemaCard.propTypes = { schemas: React.PropTypes.array.isRequired }
 
 export default SchemaCard
