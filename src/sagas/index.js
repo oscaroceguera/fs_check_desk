9939e4d7302b415135442da352c60a5b92ce1dbc
oscaroceguera <oscaroceguera@gmail.com>
2017-05-13 02:11:17 -0500

    [Sagas] ::: Import schemas in index

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,11 @@
 import { sagas as SignupSagas } from './Signup'
 import { sagas as LoginSagas } from './Login'
 import { sagas as SchemasSagas } from './Schema'
+import { sagas as SchemasListSagas } from './Schema/schemaList'
 
 export default [
   ...SignupSagas,
   ...LoginSagas,
-  ...SchemasSagas
+  ...SchemasSagas,
+  ...SchemasListSagas
 ]
