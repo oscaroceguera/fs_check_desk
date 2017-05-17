03c0a3446474657623c4df20b9cb812960ff62c6
oscaroceguera <oscaroceguera@gmail.com>
2017-05-17 01:56:15 -0500

    [Sagas] ::: Import Module single

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,10 +2,12 @@ import { sagas as SignupSagas } from './Signup'
 import { sagas as LoginSagas } from './Login'
 import { sagas as SchemasSagas } from './Schema'
 import { sagas as SchemasListSagas } from './Schema/schemaList'
+import { sagas as ModuleSingleSagas } from './Schema/moduleSingle'
 
 export default [
   ...SignupSagas,
   ...LoginSagas,
   ...SchemasSagas,
-  ...SchemasListSagas
+  ...SchemasListSagas,
+  ...ModuleSingleSagas
 ]
