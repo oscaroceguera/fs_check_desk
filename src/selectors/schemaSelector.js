f17f7bb427e920ea15d10f36a876b7a7fab30ca2
oscaroceguera <oscaroceguera@gmail.com>
2017-05-17 01:45:28 -0500

    [Selector] ::: Schema and Module validates
    - lnMajor is a length validation.
    - everyModuleItems is for show submit module form

--- src/selectors/schemaSelector.js
+++ src/selectors/schemaSelector.js
@@ -1,19 +1,30 @@
 import { createSelector } from 'reselect'
-import { every } from 'lodash/collection'
 
-const getItems = (state) => state.schemasReducer.toJS()
+const lnMajor = (item, rang) => item.length > rang
+
+/**
+ ** Schema Form
+ */
+const getSchemaItems = (state) => state.schemasReducer.toJS()
 
 const everySchema = createSelector(
-  getItems,
-  ({schema}) => every(schema)
+  getSchemaItems,
+  ({schema}) => lnMajor(schema.name, 1) && lnMajor(schema.version, 1) && lnMajor(schema.description, 0)
 )
 
-const descriptionMax140 = createSelector(
-  getItems,
-  ({schema}) => schema.description.length > 140
-)
+const descriptionMax140 = createSelector(getSchemaItems, ({schema}) => lnMajor(schema.description, 140))
 
 export const submitSchema = createSelector(
   [everySchema, descriptionMax140],
   (everySchema, max140) => everySchema && !max140
+)
+
+/**
+ ** Module Form
+ */
+const getModuleItems = (state) => state.modulesReducer.toJS()
+
+export const everyModuleItems = createSelector(
+  getModuleItems,
+  ({module}) => lnMajor(module.name, 1) && lnMajor(module.number, 0) && Number(module.order)
 )
