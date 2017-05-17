0ef66356cbe34a0857e9766d678e86fce4d30c59
oscaroceguera <oscaroceguera@gmail.com>
2017-05-17 02:04:08 -0500

    Delete ItemsForm components

--- src/components/Schema/ItemsForm.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const ItemsForm = () => (
-  <div style={{flexGrow: 1, margin: '2em 0', textAlign: 'center'}}>
-    <h1>Items</h1>
-  </div>
-)
-
-export default ItemsForm
