28f152fbaea23de48d3dcb5b51940adf12ee7c1e
oscaroceguera <oscaroceguera@gmail.com>
2017-05-17 01:49:53 -0500

    [Components] ::: Schema form was created

--- src/components/Schema/Module/ModuleForm.js
+++ src/components/Schema/Module/ModuleForm.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import TxtFieldResponsive from '../Forms/TxtFieldResponsive'
+import TxtFieldResponsive from '../../Forms/TxtFieldResponsive'
+import GenericSubmit from '../../Forms/GenericSubmit'
 
-const ModuleForm = ({ handleErrorText, onChangeInput }) => (
+const ModuleForm = ({ handleErrorText, onChangeInput, submit, onSaved }) => (
   <div style={{ display: 'flex', flexWrap: 'wrap' }}>
     <TxtFieldResponsive
       floatingLabelText={'Número'}
       name={'number'}
       width={'15%'}
-      errorText={handleErrorText('module', 'number', 'txt')}
+      errorText={handleErrorText('module', 'number', 'txtZero')}
       onChange={(e, section, type) => onChangeInput(e, 'module', 'setModuleFields')}
     />
     <TxtFieldResponsive
       floatingLabelText={'Orden'}
       name={'order'}
       width={'15%'}
-      errorText={handleErrorText('module', 'order', 'txt')}
+      errorText={handleErrorText('module', 'order', 'number')}
       onChange={(e, section, type) => onChangeInput(e, 'module', 'setModuleFields')}
     />
     <TxtFieldResponsive
@@ -23,6 +24,11 @@ const ModuleForm = ({ handleErrorText, onChangeInput }) => (
       width={'60%'}
       errorText={handleErrorText('module', 'name', 'txt')}
       onChange={(e, section, type) => onChangeInput(e, 'module', 'setModuleFields')}
+    />
+    <GenericSubmit
+      disabled={submit}
+      label={'Guardar'}
+      onClick={onSaved}
     />
   </div>
 )
