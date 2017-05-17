ea331a1a266a2dbcae5c170e19e720cf01267f8f
oscaroceguera <oscaroceguera@gmail.com>
2017-05-17 02:05:08 -0500

    [Reducer] ::: ResetFields & change order type for null

--- src/reducers/modulesReducer.js
+++ src/reducers/modulesReducer.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable'
 
 const SET_FIELDS = 'src/modules/SET_FIELDS'
-// const RESET_FIELDS = 'src/modules/RESET_FIELDS'
+const RESET_FIELDS = 'src/modules/RESET_FIELDS'
 
 export const SET_SAVED_MODULE = 'src/modules/SET_SAVED_MODULE'
 export const SET_SAVED_MODULE_LOADING = 'src/modules/SET_SAVED_MODULE_LOADING'
@@ -9,18 +9,17 @@ export const SET_SAVED_MODULE_SUCCESS = 'src/modules/SET_SAVED_MODULE_SUCCESS'
 export const SET_SAVED_MODULE_FAIL = 'src/modules/SET_SAVED_MODULE_FAIL'
 
 export const setModuleFields = (section, item, value) => ({ type: SET_FIELDS, section, item, value })
-// export const resetFields = () => ({ type: RESET_FIELDS })
+export const resetFields = () => ({ type: RESET_FIELDS })
 export const setSavedModule = () => ({ type: SET_SAVED_MODULE })
 export const setSavedModuleLoading = () => ({ type: SET_SAVED_MODULE_LOADING })
 export const setSavedModuleSuccess = (module) => ({ type: SET_SAVED_MODULE_SUCCESS, module })
 export const setSavedModuleFail = (err) => ({ type: SET_SAVED_MODULE_FAIL, err })
 
 const initialState = fromJS({
-  modules: [],
   module: {
     name: '',
     number: '',
-    order: ''
+    order: null
   },
   savedLoading: false,
   savedFail: null
@@ -30,8 +29,8 @@ function modulesReducer (state = initialState, action) {
   switch (action.type) {
     case SET_FIELDS:
       return state.setIn([action.section, action.item], action.value)
-    // case RESET_FIELDS:
-    //   return state.merge(initialState)
+    case RESET_FIELDS:
+      return state.merge(initialState)
     case SET_SAVED_MODULE_LOADING:
       return state.merge({
         savedLoading: true,
