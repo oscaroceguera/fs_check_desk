3cc3a5c2917988e3bc303dc93d2f8fb529e442ad
oscaroceguera <oscaroceguera@gmail.com>
2017-05-17 02:04:28 -0500

    [Container] ::: Module list

---
+++ src/containers/Schemas/Modules/ModuleListContainer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class ModuleListContainer extends React.Component {
+  render () {
+    return (
+      <div style={{boxShadow: '0px 0px 5px gray', fontSize: '12px', margin: '.5em 0', borderRadius: '2px', padding: '0 1em'}}>
+        <div style={{ display: 'flex'}}>
+          <p>1 -</p>
+          <p>Sitema algo de no se de la gerencia</p>
+        </div>
+        <div style={{ display: 'flex'}}>
+          <p>1 -</p>
+          <p>Sitema algo de no se de la gerencia</p>
+        </div>
+        <div style={{ display: 'flex'}}>
+          <p>1 -</p>
+          <p>Sitema algo de no se de la gerencia</p>
+        </div>
+        <div style={{ display: 'flex'}}>
+          <p>1 -</p>
+          <p>Sitema algo de no se de la gerencia</p>
+        </div>
+        <div style={{ display: 'flex'}}>
+          <p>1 -</p>
+          <p>Sitema algo de no se de la gerencia</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ModuleListContainer
