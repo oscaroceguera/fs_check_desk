f782842a46c253547843c72208b42c80a0213516
oscaroceguera <oscaroceguera@gmail.com>
2017-05-21 02:58:53 -0500

    [Sagas] ::: ModuleList was added in index

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,11 +3,13 @@ import { sagas as LoginSagas } from './Login'
 import { sagas as SchemasSagas } from './Schema'
 import { sagas as SchemasListSagas } from './Schema/schemaList'
 import { sagas as ModuleSingleSagas } from './Schema/moduleSingle'
+import { sagas as ModuleListSagas } from './Schema/moduleList'
 
 export default [
   ...SignupSagas,
   ...LoginSagas,
   ...SchemasSagas,
   ...SchemasListSagas,
-  ...ModuleSingleSagas
+  ...ModuleSingleSagas,
+  ...ModuleListSagas
 ]
