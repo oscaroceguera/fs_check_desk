496c3bed510588bf869716fc95a8f7977e9a4762
oscaroceguera <oscaroceguera@gmail.com>
2017-05-21 03:06:07 -0500

    [Helpers] ::: Post/Get module

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -22,3 +22,18 @@ export const postSchema = (data, token) =>
   axios
     .post(`${api}/auth/schema`, data, headers(token))
     .then((response) => response.data)
+
+export const postModule = (data, token) =>
+  axios
+    .post(`${api}/auth/module`, data, headers(token))
+    .then((response) => response.data)
+
+export const getModulesBySchemaId = (schemaId, token) =>
+  axios
+    .get(`${api}/auth/modules/${schemaId}`, headers(token))
+    .then((items) => items.data)
+
+// export const getModulesBySchemaId = (schemaId = '8432ecb8-f371-491e-9b05-3f5424a9f02c', token) =>
+//   axios
+//     .get(`${api}/auth/modules/${schemaId}`, headers(token))
+//     .then((items) => items.data)
