8a48e4db679acc93799def4a2c81793fde1c9fc4
oscaroceguera <oscaroceguera@gmail.com>
2017-05-21 03:09:30 -0500

    [Components] ::: Form was created

--- src/components/Schema/Item/ItemsForm.js
+++ src/components/Schema/Item/ItemsForm.js
@@ -1,8 +1,92 @@
-// import React from 'react'
-// const ItemsForm = () => (
-//   <div style={{flexGrow: 1, margin: '2em 0', textAlign: 'center'}}>
-//     <h1>Items</h1>
-//   </div>
-// )
-//
-// export default ItemsForm
+import React from 'react'
+import {SelectField, MenuItem} from 'material-ui'
+import TxtFieldResponsive from '../../Forms/TxtFieldResponsive'
+import styled from 'styled-components'
+import GenericSubmit from '../../Forms/GenericSubmit'
+
+const Title = styled.h1`
+  margin: 0;
+  color: gray;
+  text-align: center;
+`
+
+const FormContainer = styled.div`
+  padding: .5em;
+  box-shadow: 0px 0px 5px gray;
+  margin-top: 1em;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const ItemsForm = ({item, modulesTypes, selectChange, handleErrorText, onChangeInput, submit, onSaved}) => (
+  <div>
+    <Title>{'Administrar Reactivos'}</Title>
+    <FormContainer>
+      <SelectField
+        errorText={handleErrorText('item', 'moduleId', 'txt')}
+        floatingLabelText={'Módulo'}
+        value={item.moduleId}
+        onChange={selectChange}
+        fullWidth
+      >
+        { modulesTypes.map((i,k) => <MenuItem key={k} value={i._id} primaryText={i.name} />) }
+      </SelectField>
+      <TxtFieldResponsive
+        floatingLabelText={'Número'}
+        name={'number'}
+        width={'15%'}
+        errorText={handleErrorText('item', 'number', 'txtZero')}
+        onChange={(e, section) => onChangeInput(e, 'item')}
+      />
+      <TxtFieldResponsive
+        floatingLabelText={'Orden'}
+        name={'order'}
+        width={'15%'}
+        errorText={handleErrorText('item', 'order', 'number')}
+        onChange={(e, section, type) => onChangeInput(e, 'item')}
+      />
+      <TxtFieldResponsive
+        floatingLabelText={'Valor'}
+        name={'value'}
+        width={'15%'}
+        errorText={handleErrorText('item', 'value', 'number')}
+        onChange={(e, section, type) => onChangeInput(e, 'item')}
+      />
+      <TxtFieldResponsive
+        floatingLabelText={'Pregunta'}
+        name={'answer'}
+        width={'100%'}
+        errorText={handleErrorText('item', 'answer', 'txt')}
+        onChange={(e, section, type) => onChangeInput(e, 'item')}
+        multiLine={true}
+        rows={2}
+      />
+      <TxtFieldResponsive
+        floatingLabelText={'Métrica'}
+        name={'recommend'}
+        width={'100%'}
+        errorText={handleErrorText('item', 'recommend', 'txt')}
+        onChange={(e, section, type) => onChangeInput(e, 'item')}
+        multiLine={true}
+        rows={2}
+      />
+      <GenericSubmit
+        disabled={submit}
+        label={'Guardar'}
+        onClick={onSaved}
+      />
+    </FormContainer>
+  </div>
+)
+
+ItemsForm.propTypes = {
+  item: React.PropTypes.object.isRequired,
+  modulesTypes: React.PropTypes.array.isRequired,
+  selectChange: React.PropTypes.func.isRequired,
+  handleErrorText: React.PropTypes.func.isRequired,
+  onChangeInput: React.PropTypes.func.isRequired,
+  submit: React.PropTypes.bool.isRequired,
+  onSaved: React.PropTypes.func.isRequired
+}
+
+export default ItemsForm
