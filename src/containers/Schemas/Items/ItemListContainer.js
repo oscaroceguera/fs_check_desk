b4a56f2922ebd3185f0df2d3b54de5192fd8f3eb
oscaroceguera <oscaroceguera@gmail.com>
2017-06-02 01:59:39 -0500

    [Container] ::: import item list component

--- src/containers/Schemas/Items/ItemListContainer.js
+++ src/containers/Schemas/Items/ItemListContainer.js
@@ -2,57 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as itemsActions from '../../../reducers/getItemsReducer'
-import Divider from 'material-ui/Divider'
+import ItemList from '../../../components/Schema/Item/ItemList'
 
 class ItemListContainer extends React.Component {
-  componentWillMount () {
-    this.props.fetchItems()
-  }
+  componentWillMount () { this.props.fetchItems() }
 
   render () {
-    return (
-      <div style={{paddingLeft: '2em'}}>
-        <h3 style={{color: 'gray', textAlign: 'center'}}>{'Reactivos'}</h3>
-        <div style={{boxShadow: '0px 0px 5px gray'}}>
-          <div style={{display: 'flex', textAlign: 'center', color: 'gray', padding: '0 .5em'}}>
-            <p style={{width: '20%'}}>{'Módulo'}</p>
-            <p style={{width: '35%'}}>{'Pregunta'}</p>
-            <p style={{width: '35%'}}>{'Métrica'}</p>
-            <p style={{width: '10%'}}>{'Valor'}</p>
-          </div>
-          <Divider />
-          {
-            this.props.items.map((item, index) => {
-              return (
-                <div key={index}>
-                  <div style={{display: 'flex', fontSize: '12px', padding: '0.5em'}}>
-                    <p style={{width: '20%', margin: 0, padding: '.5em'}}>{item.moduleId.name}</p>
-                    <p style={{width: '35%', margin: 0, padding: '.5em'}}>{item.answer}</p>
-                    <p style={{width: '35%', margin: 0, padding: '.5em'}}>{item.recommend}</p>
-                    <p style={{width: '10%', margin: 0, padding: '.5em', textAlign: 'center'}}>{item.value}</p>
-                  </div>
-                  <Divider />
-                </div>
-              )
-            })
-          }
-        </div>
-      </div>
-    )
+    return <ItemList items={this.props.items}/>
   }
 }
 
-const mapStateToProps = (state) => {
-  const itemsJS = state.getItemsReducer.toJS()
-  return {
-    items: itemsJS.items
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    ...itemsActions
-  }, dispatch)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ItemListContainer)
+export default connect(
+  state => ({ items: state.getItemsReducer.toJS().items }),
+  dispatch => bindActionCreators({ ...itemsActions }, dispatch)
+)(ItemListContainer)
