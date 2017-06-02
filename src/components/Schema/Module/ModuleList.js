dfb63868d2c21e44cf90d0c2aeaf235798924862
oscaroceguera <oscaroceguera@gmail.com>
2017-06-02 01:56:08 -0500

    [Component] ::: Edit option was added

--- src/components/Schema/Module/ModuleList.js
+++ src/components/Schema/Module/ModuleList.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Divider from 'material-ui/Divider'
 import styled from 'styled-components'
+import CreateIcon from 'material-ui/svg-icons/content/create';
+
+const ModuleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
 const NotModulesMsg = styled.p`
   padding: 1em;
@@ -12,6 +19,14 @@ const ModuleItem = styled.div`
   padding: .5em;
 `
 
+const ItemOption = styled.div`
+  margin-right: .5em;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`
+
 const ModuleItemSection = ({title, label}) => <p style={{ margin: 0 }}><strong>{title}</strong>{label}</p>
 
 ModuleItemSection.propTypes = {
@@ -21,18 +36,24 @@ ModuleItemSection.propTypes = {
 
 export const NotModules = () => <NotModulesMsg>{'¡No hay módulos registrados!'}</NotModulesMsg>
 
-const ModuleList = ({item}) => (
+const ModuleList = ({item, modalUpdate}) => (
   <div>
-    <ModuleItem>
-      <ModuleItemSection title={'Módulo: '} label={item.number}/>
-      <ModuleItemSection title={'Nombre: '} label={item.name}/>
-    </ModuleItem>
+    <ModuleContainer>
+      <ModuleItem>
+        <ModuleItemSection title={'Módulo: '} label={item.number}/>
+        <ModuleItemSection title={'Nombre: '} label={item.name}/>
+      </ModuleItem>
+      <ItemOption onClick={(e, module) => modalUpdate(e,item)}>
+        <CreateIcon style={{ color: '#757575' }}/>
+      </ItemOption>
+    </ModuleContainer>
     <Divider />
   </div>
 )
 
 ModuleList.propTypes = {
-  item: React.PropTypes.object.isRequired
+  item: React.PropTypes.object.isRequired,
+  modalUpdate: React.PropTypes.func.isRequired
 }
 
 export default ModuleList
