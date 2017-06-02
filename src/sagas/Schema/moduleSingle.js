d6456f663fc7ea06a0f7b6e504d6fca99060b190
oscaroceguera <oscaroceguera@gmail.com>
2017-06-02 01:49:36 -0500

    [Sagas] ::: ShowModal & UpdateModule
    - ShowModal : Show modal For update module
    - UpdateModule : Update Form for update module

--- src/sagas/Schema/moduleSingle.js
+++ src/sagas/Schema/moduleSingle.js
@@ -1,15 +1,16 @@
 import { delay } from 'redux-saga'
 import { call, put, select, takeLatest } from 'redux-saga/effects'
 import {
-  SET_SAVED_MODULE,
+  SET_SAVED_MODULE, SHOW_MODAL_UPDATE_MODULE, SET_UPDATE_MODULE,
   setSavedModuleLoading,
   setSavedModuleSuccess,
   setSavedModuleFail,
   resetFields
 } from '../../reducers/modulesReducer'
 import { fetchModules } from '../../reducers/getModulesReducer'
-import { closeModal } from '../../reducers/modalReducer'
-import { postModule } from '../../helpers/api'
+import { fetchItems } from '../../reducers/getItemsReducer'
+import { closeModal, openModal } from '../../reducers/modalReducer'
+import { postModule, putModule } from '../../helpers/api'
 
 function* savedModule () {
   yield put(setSavedModuleLoading())
@@ -28,9 +29,32 @@ function* savedModule () {
   }
 }
 
+function* modalUpdateModule (action) {
+  yield put(setSavedModuleSuccess(action.module))
+  yield put(openModal())
+}
+
+function* updateModule() {
+  yield put(setSavedModuleLoading())
+  const data = yield select((state) => state.modulesReducer.toJS().module)
+  yield delay(1000)
+  try {
+    const module = yield call(putModule, data.id, data, localStorage.getItem('token'))
+    yield put(setSavedModuleSuccess(module))
+    yield put(fetchModules())
+    yield put(fetchItems())
+    yield put(resetFields())
+    yield put(closeModal())
+  } catch (err) {
+    yield put(setSavedModuleFail(err))
+  }
+}
+
 function* defaultSaga () {
   yield [
-    takeLatest(SET_SAVED_MODULE, savedModule)
+    takeLatest(SET_SAVED_MODULE, savedModule),
+    takeLatest(SHOW_MODAL_UPDATE_MODULE, modalUpdateModule),
+    takeLatest(SET_UPDATE_MODULE, updateModule)
   ]
 }
 
