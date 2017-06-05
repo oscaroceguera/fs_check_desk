7df7f3781c64e31cf1967cffc1ff61b990505cd3
oscaroceguera <oscaroceguera@gmail.com>
2017-06-05 02:34:49 -0500

    [Component] ::: Linear progress was created

---
+++ src/components/Progress/LinearProgress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LinearProgress from 'material-ui/LinearProgress';
+import './style.css'
+
+const LnProgress = () => (
+  <div className='linearContainer'>
+    <LinearProgress mode="indeterminate" />
+  </div>
+)
+
+export default LnProgress
