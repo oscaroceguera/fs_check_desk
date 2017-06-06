6443331fc2b649667905c94a8083b004000da4ba
oscaroceguera <oscaroceguera@gmail.com>
2017-06-05 22:56:21 -0500

    Items List ::: Open modal option was added

--- src/components/Schema/Item/ItemList.js
+++ src/components/Schema/Item/ItemList.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Divider from 'material-ui/Divider'
 import CircularLoading from '../../Progress/CircularLoading'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import Modal from '../../Modal/Modal'
+import ItemFormContainer from '../../../containers/Schemas/Items/ItemFormContainer'
 import {
   Container, Title, Table,
   TableHeader, RowHeaderStyled,
   TableBodyStyled, RowBodyStyled,
-  NotItemsMsg
+  NotItemsMsg, TitleContainer
 } from './styles'
 
 
@@ -25,16 +29,37 @@ const TableBody = ({item}) => (
 
 TableBody.propTypes = { Items: React.PropTypes.array }
 
-const ItemList = ({items, loading}) => (
-  <Container>
+const TopTitle = ({onOpenForm}) => (
+  <TitleContainer>
     <Title>{'Reactivos'}</Title>
+    <FloatingActionButton
+      onClick={onOpenForm}
+      secondary={true}
+      mini
+    >
+      <ContentAdd />
+    </FloatingActionButton>
+  </TitleContainer>
+)
+
+TopTitle.propTypes = { onOpenForm: React.PropTypes.func.isRequired }
+
+const TitleHeaderContainer = () => (
+  <TableHeader>
+    <RowHeaderStyled width={'20%'}>{'Módulo'}</RowHeaderStyled>
+    <RowHeaderStyled width={'35%'}>{'Pregunta'}</RowHeaderStyled>
+    <RowHeaderStyled width={'35%'}>{'Métrica'}</RowHeaderStyled>
+    <RowHeaderStyled width={'10%'}>{'Valor'}</RowHeaderStyled>
+  </TableHeader>
+)
+
+const ModalContainer = (props) => <Modal {...props}><ItemFormContainer /></Modal>
+
+const ItemList = ({items, loading, onOpenForm, onCloseForm, modalStatus}) => (
+  <Container>
+    <TopTitle onOpenForm={onOpenForm}/>
     <Table>
-      <TableHeader>
-        <RowHeaderStyled width={'20%'}>{'Módulo'}</RowHeaderStyled>
-        <RowHeaderStyled width={'35%'}>{'Pregunta'}</RowHeaderStyled>
-        <RowHeaderStyled width={'35%'}>{'Métrica'}</RowHeaderStyled>
-        <RowHeaderStyled width={'10%'}>{'Valor'}</RowHeaderStyled>
-      </TableHeader>
+      <TitleHeaderContainer />
       <Divider />
       {
         loading
@@ -44,12 +69,21 @@ const ItemList = ({items, loading}) => (
             : <NotItems />
       }
     </Table>
+    <ModalContainer
+      handleModalOpen={onOpenForm}
+      handleModalClose={onCloseForm}
+      modalStatus={modalStatus}
+      title={'Agregar reactivo'}
+    />
   </Container>
 )
 
 ItemList.propTypes = {
   Items: React.PropTypes.array,
-  loading: React.PropTypes.bool.isRequired
+  loading: React.PropTypes.bool.isRequired,
+  onOpenForm: React.PropTypes.func.isRequired,
+  onCloseForm: React.PropTypes.func.isRequired,
+  modalStatus: React.PropTypes.bool.isRequired
 }
 
 export default ItemList
--- src/containers/Schemas/Items/ItemListContainer.js
+++ src/containers/Schemas/Items/ItemListContainer.js
@@ -2,20 +2,37 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as itemsActions from '../../../reducers/getItemsReducer'
+import * as itemActions from '../../../reducers/itemsReducer'
 import ItemList from '../../../components/Schema/Item/ItemList'
 
 class ItemListContainer extends React.Component {
-  componentWillMount () { this.props.fetchItems() }
+  componentWillMount () {
+    this.props.fetchItems()
+  }
 
   render () {
-    const { items, loading } = this.props
-    return <ItemList items={items} loading={loading}/>
+    return (
+      <ItemList
+        {...this.props}
+        onOpenForm={() => this.props.openForm()}
+        onCloseForm={() => this.props.closeForm()}
+      />
+    )
   }
 }
 
 // TODO: PROPTYPES
 
+const mapStateToProps = (state, props) => ({
+  items: state.getItemsReducer.toJS().items,
+  loading: props.itemsLoading,
+  modalStatus: state.itemsReducer.toJS().openFormModal
+})
+
 export default connect(
-  (state, props) => ({ items: state.getItemsReducer.toJS().items, loading: props.itemsLoading }),
-  dispatch => bindActionCreators({ ...itemsActions }, dispatch)
+  mapStateToProps,
+  dispatch => bindActionCreators({
+    ...itemsActions,
+    ...itemActions
+  }, dispatch)
 )(ItemListContainer)
