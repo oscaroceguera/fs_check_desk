97d2c063f8b8d6407a3a0123ae2f6c742738e232
oscaroceguera <oscaroceguera@gmail.com>
2017-06-08 02:39:07 -0500

    [Sagas] ::: Remove Item

--- src/sagas/Schema/itemList.js
+++ src/sagas/Schema/itemList.js
@@ -1,16 +1,26 @@
 import { select, takeEvery } from 'redux-saga/effects'
-import { getItemsBySchemaId } from '../../helpers/api'
-import { FETCH_ITEMS, fetchItemsLoading, fetchItemsSuccess, fetchItemsFail } from '../../reducers/getItemsReducer'
-import { fetchApiSaga } from '../commons/genericSagas'
+import { getItemsBySchemaId, deleteItemById } from '../../helpers/api'
+import {
+  FETCH_ITEMS, SET_DELETE_ITEM,
+  fetchItemsLoading, fetchItemsSuccess, fetchItemsFail,
+  setDeleteItemSuccess, setDeleteItemFail
+} from '../../reducers/getItemsReducer'
+import { fetchApiSaga, deleteApiSaga } from '../commons/genericSagas'
 
 function* fetchItems () {
   const schemaId = yield select(state => state.schemasReducer.toJS().schema.id)
   yield* fetchApiSaga(getItemsBySchemaId, [schemaId, localStorage.getItem('token')], fetchItemsLoading, fetchItemsSuccess, fetchItemsFail)
 }
 
+function* removeItem (action) {
+  const { id, index } = action
+  yield* deleteApiSaga(deleteItemById, [id, localStorage.getItem('token')], index, fetchItemsLoading, setDeleteItemSuccess, setDeleteItemFail)
+}
+
 function* defaultSaga () {
   yield [
-    takeEvery(FETCH_ITEMS, fetchItems)
+    takeEvery(FETCH_ITEMS, fetchItems),
+    takeEvery(SET_DELETE_ITEM, removeItem)
   ]
 }
 
