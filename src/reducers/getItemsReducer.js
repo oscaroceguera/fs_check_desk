b21125b91ffd86dd5bcde5f18e53401f0c80e161
oscaroceguera <oscaroceguera@gmail.com>
2017-06-08 02:41:43 -0500

    [Reducer] getItemsReducer : delete item was added

--- src/reducers/getItemsReducer.js
+++ src/reducers/getItemsReducer.js
@@ -5,15 +5,24 @@ export const FETCH_ITEMS_LOADING = 'src/item/FETCH_ITEMS_LOADING'
 export const FETCH_ITEMS_SUCCESS = 'src/item/FETCH_ITEMS_SUCCESS'
 export const FETCH_ITEMS_FAIL = 'src/item/FETCH_ITEMS_FAIL'
 
+export const SET_DELETE_ITEM = 'src/item/SET_DELETE_ITEM'
+export const SET_DELETE_ITEM_SUCCESS = 'src/item/SET_DELETE_ITEM_SUCCESS'
+export const SET_DELETE_ITEM_FAIL = 'src/item/SET_DELETE_ITEM_FAIL'
+
 export const fetchItems = () => ({ type: FETCH_ITEMS })
 export const fetchItemsLoading = () => ({ type: FETCH_ITEMS_LOADING })
 export const fetchItemsSuccess = (items) => ({ type: FETCH_ITEMS_SUCCESS, items })
 export const fetchItemsFail = (err) => ({ type: FETCH_ITEMS_FAIL, err })
 
+export const setDeleteItem = (id, index) => ({ type: SET_DELETE_ITEM, id, index })
+export const setDeleteItemSuccess = (index) => ({ type: SET_DELETE_ITEM_SUCCESS, index })
+export const setDeleteItemFail = (err) => ({ type: SET_DELETE_ITEM_FAIL, err })
+
 const initialState = fromJS({
   items: [],
   itemsLoading: false,
-  itemsFail: null
+  itemsFail: null,
+  msgRemoveFail: null
 })
 
 function getItemsReducer (state = initialState, action) {
@@ -32,6 +41,15 @@ function getItemsReducer (state = initialState, action) {
       return state.merge({
         itemsLoading: false,
         itemsFail: action.err
+      })
+    case SET_DELETE_ITEM_SUCCESS:
+      return state
+        .deleteIn(['items', action.index])
+        .set('itemsLoading', false)
+    case SET_DELETE_ITEM_FAIL:
+      return state.merge({
+        msgRemoveFail: action.err,
+        itemsLoading: false
       })
     default:
       return state
