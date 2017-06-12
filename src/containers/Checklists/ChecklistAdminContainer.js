e7725ee244695a4f40bf1760a172cbf4b6cb5cc1
oscaroceguera <oscaroceguera@gmail.com>
2017-06-11 22:15:56 -0500

    [Container] ChecklistAdminContainer was added

---
+++ src/containers/Checklists/ChecklistAdminContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import DashboardWrapper from '../../components/Wrappers/DashboardWrapper'
+import ChecklistFormContainer from './ChecklistFormContainer'
+
+// TODO: 4 Al guardar el checklist mostar los reactivos
+// TODO: 7 Cierre de checklist
+// TODO: 8 Mostrar estadus de checklist como cerrado
+// TODO: 9 Cuando el checkist este cerradoe ste ya no se podra modificar
+// TODO: Prop types
+
+class ChecklistAdminContainer extends React.Component {
+  render () {
+    return (
+      <DashboardWrapper
+        title={'Check-lists'}
+        desc={'Aplicar Check-list'}
+      >
+        <ChecklistFormContainer />
+      </DashboardWrapper>
+    )
+  }
+}
+
+export default ChecklistAdminContainer
