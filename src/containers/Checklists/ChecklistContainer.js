f95548e4b5cf5b75f37fcaa8fdf29e4de64e6e0a
oscaroceguera <oscaroceguera@gmail.com>
2017-06-11 23:18:01 -0500

    [Container] CheckListContainer : Init container

---
+++ src/containers/Checklists/ChecklistContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// TODO: 5 Guardar reactivo por individual
+// TODO: 6 Opción de actualizar cuando este este guardado
+// TODO: Prop types
+
+class ChecklistContainer extends React.Component {
+  render () {
+    return <h1>Items</h1>
+  }
+}
+
+export default ChecklistContainer
