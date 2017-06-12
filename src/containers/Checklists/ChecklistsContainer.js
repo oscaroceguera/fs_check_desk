3386a1c3d86d2d9914162c3145730de8f0d5d7ab
oscaroceguera <oscaroceguera@gmail.com>
2017-06-11 23:16:02 -0500

    [Container] ChecklistContainer : Add DashBoardWarpper & AddButton

--- src/containers/Checklists/ChecklistsContainer.js
+++ src/containers/Checklists/ChecklistsContainer.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as actions from '../../reducers/getChecklistReducer'
+import DashboardWrapper from '../../components/Wrappers/DashboardWrapper'
+import AddButton from '../../components/AddButton/AddButton'
 
-const ChecklistsContainer = () => (
-  <h1>Checklist</h1>
-)
+class ChecklistsContainer extends React.Component {
+  render () {
+    return (
+      <DashboardWrapper
+        title={'Check-lists'}
+        desc={'Admistrador de check-lists'}
+      >
+        <h1>Lista de Items</h1>
+        <AddButton goTo={(e) => this.props.goToAddNewChecklist()} />
+      </DashboardWrapper>
+    )
+  }
+}
 
-export default ChecklistsContainer
+export default connect(
+  null,
+  dispatch => bindActionCreators({...actions}, dispatch)
+)(ChecklistsContainer)
+
+// TODO: UI LIST CHECKLIST
+// TODO: GO TO CHECKLIST DETAIL
+// TODO: PROP-TYPES
+// TODO: Mostrar el status del checklist (Editable o Cerrado)
