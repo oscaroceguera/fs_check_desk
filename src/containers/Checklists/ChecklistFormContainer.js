eba92ed5560cd5d28ec3626ae2d654fa6d1cd798
oscaroceguera <oscaroceguera@gmail.com>
2017-06-11 23:18:27 -0500

    [Container] ChecklistForm was initialized

---
+++ src/containers/Checklists/ChecklistFormContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+// TODO: UI de ChecklistAdminContainer
+// TODO: 1 Seleccionar el tipo de check-list
+// TODO: 2 Información del checklist:
+// TODO: 2.1 Nombre de la empresa *isRequired
+// TODO: 2.2 Dirección:
+// TODO: 2.2.1 Pais *isRequired
+// TODO: 2.2.2 Estado *isRequired
+// TODO: 2.2.3 Municipio *isRequired
+// TODO: 2.2.4 Calle *isRequired
+// TODO: 2.2.5 Numero
+// TODO: 2.2.6 Colonia
+// TODO: 2.2.7 CP
+// TODO: 2.3 Fecha del checkList *isRequired
+// TODO: 2.4 Descripción
+// TODO: 3 Guardar checklist y obtener el Id
+// TODO: Prop types
+
+class ChecklistFormContainer extends React.Component {
+  render () {
+    return (
+      <h1>Formulario</h1>
+    )
+  }
+}
+
+export default ChecklistFormContainer
