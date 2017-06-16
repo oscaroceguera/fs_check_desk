8f077c11ac3452f51dc6d27c5caa930f74340a43
oscaroceguera <oscaroceguera@gmail.com>
2017-06-16 18:06:57 -0500

    [Reducer] Selected schema action was added

--- src/reducers/Checklist/checklistForm.js
+++ src/reducers/Checklist/checklistForm.js
@@ -2,13 +2,15 @@ import { fromJS } from 'immutable'
 
 const SET_FIELDS = 'src/checklist/SET_FIELDS'
 const RESET_FIELDS = 'src/checklist/RESET_FIELDS'
+const SELECTED_SCHEMA = 'scr/checklist/SELECTED_SCHEMA'
 
 export const setFields = (section, item, value) => ({ type: SET_FIELDS, section, item, value })
 export const resetFields = () => ({ type: RESET_FIELDS })
+export const selectedSchema = (schemaType) => ({ type: SELECTED_SCHEMA, schemaType })
 
 const initialState = fromJS({
   checklist: {
-    checklistType: '',
+    schemaType: '',
     companyName: '',
     country: '',
     state: '',
@@ -28,6 +30,8 @@ const checklistReducer = (state = initialState, action) => {
       return state.setIn([action.section, action.item], action.value)
     case RESET_FIELDS:
       return state.merge(initialState)
+    case SELECTED_SCHEMA:
+      return state.setIn(['checklist', 'schemaType'], action.schemaType)
     default:
       return state
   }
