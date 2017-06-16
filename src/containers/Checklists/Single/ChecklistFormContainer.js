4960930f9f46dac006f32f600495f4d3220cbdab
oscaroceguera <oscaroceguera@gmail.com>
2017-06-16 18:19:15 -0500

    [Container] connect with checklist & schema reducers

--- src/containers/Checklists/Single/ChecklistFormContainer.js
+++ src/containers/Checklists/Single/ChecklistFormContainer.js
@@ -1,29 +1,54 @@
 import React from 'react'
+// import { object, bool, func } from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as checklistActions from '../../../reducers/Checklist/checklistForm'
+import * as schemaActions from '../../../reducers/Schema/schemaList'
+import aux from '../../../helpers/AuxFunctions'
+
 import { ChecklistForm } from '../../../components'
 
-// TODO: UI de ChecklistAdminContainer
-// TODO: 1 Seleccionar el tipo de check-list
-// TODO: 2 Información del checklist:
-// TODO: 2.1 Nombre de la empresa *isRequired
-// TODO: 2.2 Dirección:
-// TODO: 2.2.1 Pais *isRequired
-// TODO: 2.2.2 Estado *isRequired
-// TODO: 2.2.3 Municipio *isRequired
-// TODO: 2.2.4 Calle *isRequired
-// TODO: 2.2.5 Numero
-// TODO: 2.2.6 Colonia
-// TODO: 2.2.7 CP
-// TODO: 2.3 Fecha del checkList *isRequired
-// TODO: 2.4 Descripción
 // TODO: 3 Guardar checklist y obtener el Id
 // TODO: Prop types
 
 class ChecklistFormContainer extends React.Component {
+  componentWillMount () {
+    this.props.fetchSchemas()
+  }
+
+  handleErrorText = (section, field, type) => {
+    const _section = this.props[section]
+    return aux.errorTextMessage(_section[field], type)
+  }
+
   render () {
     return (
-      <ChecklistForm />
+      <ChecklistForm
+        {...this.props}
+        selectChange={(event, index, value) => this.props.selectedSchema(value)}
+        handleErrorText={this.handleErrorText}
+        onChangeInput={(e, section) => this.props.setFields(section, e.target.name, e.target.value)}
+      />
     )
   }
 }
 
-export default ChecklistFormContainer
+const mapStateToProps = (state) => {
+  const checklist = state.checklistReducer.toJS()
+  const schemas = state.getSchemasReducer.toJS()
+  return {
+    checklist: checklist.checklist,
+    catalog: schemas.schemas,
+    // submitSchema: !submitSchema(state),
+    // SSLoading: stateJS.savedSchemasLoading
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    ...checklistActions,
+    ...schemaActions
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChecklistFormContainer)
