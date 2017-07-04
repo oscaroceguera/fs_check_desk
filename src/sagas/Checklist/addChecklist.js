7a66982d28cbc975ada9ee38b52ec4e76c4b02ea
oscaroceguera <oscaroceguera@gmail.com>
2017-07-04 00:35:14 -0500

    [Sagas] addChecklist : UpdateChecklist was added

--- src/sagas/Checklist/addChecklist.js
+++ src/sagas/Checklist/addChecklist.js
@@ -1,7 +1,7 @@
-// import { delay } from 'redux-saga'
-import { takeLatest } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+import { put, call, takeLatest } from 'redux-saga/effects'
 import * as CR from '../../reducers/Checklist/checklistForm'
-import { postChecklist } from '../../helpers/api'
+import { postChecklist, putChecklist } from '../../helpers/api'
 import { addApiSaga } from '../commons/genericSagas'
 import { getSimpleState } from '../commons/genericSelect'
 
@@ -17,9 +17,36 @@ function* savedChecklist () {
     CR.setSavedChecklistFail)
 }
 
+function* updateChecklist () {
+  yield put(CR.setLoading())
+  const data = yield getSimpleState('checklistReducer', 'checklist')
+  yield delay(1000)
+  try {
+    const checklist = yield call(putChecklist, data.id, data, localStorage.getItem('token'))
+    const _checklist = {
+      id: checklist._id,
+      schemaType: checklist.schemaType,
+      companyName: checklist.companyName,
+      country: checklist.country,
+      state: checklist.state,
+      town: checklist.town,
+      street: checklist.street,
+      number: checklist.number,
+      neighborhood: checklist.neighborhood,
+      zipcode: checklist.zipcode,
+      date: checklist.date,
+      description: checklist.description
+    }
+    yield put(CR.setUpdateChecklistSuccess(_checklist))
+  } catch (err) {
+    yield put(CR.setSavedChecklistFail())
+  }
+}
+
 function* defaultSaga () {
   yield [
-    takeLatest(CR.SET_SAVED_CHECKLIST, savedChecklist)
+    takeLatest(CR.SET_SAVED_CHECKLIST, savedChecklist),
+    takeLatest(CR.SET_UPDATE_CHECKLIST, updateChecklist)
   ]
 }
 
