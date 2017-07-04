e94d9ae73da31a0a571e354c6430084becb64896
oscaroceguera <oscaroceguera@gmail.com>
2017-07-04 00:36:12 -0500

    [Reducer] ChecklistForm : UpdateChecklist actions were added

--- src/reducers/Checklist/checklistForm.js
+++ src/reducers/Checklist/checklistForm.js
@@ -1,20 +1,37 @@
 import { fromJS } from 'immutable'
 
+// FIELDS
 const SET_FIELDS = 'src/checklist/SET_FIELDS'
 const RESET_FIELDS = 'src/checklist/RESET_FIELDS'
-const SELECTED_SCHEMA = 'src/checklist/SELECTED_SCHEMA'
-export const SET_SAVED_CHECKLIST = 'src/checklist/SET_SAVED_CHECKLIST'
-const SET_LOADING = 'src/checklist/SET_LOADING'
-const SET_SAVED_CHECKLIST_FAIL = 'src/checklist/SET_SAVED_CHECKLIST_FAIL'
-const SET_SAVED_CHECKLIST_SUCCESS = 'src/checklist/SET_SAVED_CHECKLIST_SUCCESS'
 
 export const setFields = (section, item, value) => ({ type: SET_FIELDS, section, item, value })
 export const resetFields = () => ({ type: RESET_FIELDS })
+
+// SELECTED SCHEMA
+const SELECTED_SCHEMA = 'src/checklist/SELECTED_SCHEMA'
+
 export const selectedSchema = (schemaType) => ({ type: SELECTED_SCHEMA, schemaType })
-export const setSavedChecklist = () => ({ type: SET_SAVED_CHECKLIST })
+
+// LOADING
+const SET_LOADING = 'src/checklist/SET_LOADING'
+
 export const setLoading = () => ({ type: SET_LOADING })
+
+// SET SAVED CHECKLIST
+export const SET_SAVED_CHECKLIST = 'src/checklist/SET_SAVED_CHECKLIST'
+const SET_SAVED_CHECKLIST_SUCCESS = 'src/checklist/SET_SAVED_CHECKLIST_SUCCESS'
+const SET_SAVED_CHECKLIST_FAIL = 'src/checklist/SET_SAVED_CHECKLIST_FAIL'
+
+export const setSavedChecklist = () => ({ type: SET_SAVED_CHECKLIST })
 export const setSavedChecklistSuccess = (checklist) => ({ type: SET_SAVED_CHECKLIST_SUCCESS, checklist })
 export const setSavedChecklistFail = (err) => ({ type: SET_SAVED_CHECKLIST_FAIL, err })
+
+// UPDATE CHECKLIST
+export const SET_UPDATE_CHECKLIST = 'src/checklist/SET_UPDATE_CHECKLIST'
+const SET_UPDATE_CHECKLIST_SUCCESS = 'src/checklist/SET_UPDATE_SCHEMA_SUCCESS'
+
+export const setUpdateChecklist = () => ({ type: SET_UPDATE_CHECKLIST })
+export const setUpdateChecklistSuccess = (checklist) => ({ type: SET_UPDATE_CHECKLIST_SUCCESS, checklist })
 
 const initialState = fromJS({
   checklist: {
@@ -44,6 +61,7 @@ const checklistReducer = (state = initialState, action) => {
       return state.setIn(['checklist', 'schemaType'], action.schemaType)
     case SET_LOADING:
       return state.set('loading', true)
+    case SET_UPDATE_CHECKLIST_SUCCESS:
     case SET_SAVED_CHECKLIST_SUCCESS:
       return state.merge({
         checklist: action.checklist,
